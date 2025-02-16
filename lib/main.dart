98732f36425e2fe97fe18fdc108b6103842a2aa3
mynul5837 <islam15-5837@diu.edu.bd>
2025-02-16 10:08:39 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 void main() {
   runApp(const MyLAB01());
@@ -9,6 +10,51 @@ class MyLAB01 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("TheFirstApp"),
+          backgroundColor:
+              const Color.fromARGB(255, 133, 106, 106), // Dark navigation bar
+        ),
+        body: Column(
+          children: [
+            const Text(
+              "4.0",
+              style: TextStyle(color: Colors.black, fontSize: 30),
+            ), // Gap between text and stars
+            Row(
+              children: const [
+                Icon(
+                  Icons.star,
+                  size: 50,
+                  color: Colors.black,
+                ),
+                Icon(
+                  Icons.star,
+                  size: 50,
+                  color: Colors.black,
+                ),
+                Icon(
+                  Icons.star,
+                  size: 50,
+                  color: Colors.black,
+                ),
+                Icon(
+                  Icons.star,
+                  size: 50,
+                  color: Colors.black,
+                ),
+                Icon(
+                  Icons.star_border,
+                  size: 50,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
