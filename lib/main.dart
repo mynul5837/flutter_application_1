b87a0b46baa56d230792fbaafb0a95089bf27d3d
mynul5837 <islam15-5837@diu.edu.bd>
2025-02-16 09:24:36 +0600

    initialize

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyLAB01());
+}
+
+class MyLAB01 extends StatelessWidget {
+  const MyLAB01({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
